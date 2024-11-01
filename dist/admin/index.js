f716fcf8294712adbc443f651fac47ed19b21716
JakubVavra <69152298+JakubVavra@users.noreply.github.com>
2024-11-01 13:40:14 +0100

    Fix

--- dist/admin/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const index = require("../_chunks/index-C1a7nMc0.js");
-require("@strapi/icons");
-module.exports = index.index;
-//# sourceMappingURL=index.js.map
--- dist/admin/index.mjs
+++
@@ -1,6 +0,0 @@
-import { i } from "../_chunks/index-NsIZL7Qx.mjs";
-import "@strapi/icons";
-export {
-  i as default
-};
-//# sourceMappingURL=index.mjs.map
